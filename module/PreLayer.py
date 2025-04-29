474a84c8de779451633f335205a0a7538c8d1723
sonrisa <sonder0724@gmail.com>
2025-04-29 11:26:57 +0800

    4.29

--- module/PreLayer.py
+++ module/PreLayer.py
@@ -2,7 +2,6 @@ import torch.nn as nn
 
 
 class PreLayer(nn.Module):
-
     def __init__(self, d, hidden: list[int]):
         super(PreLayer, self).__init__()
         assert len(hidden) > 0
@@ -10,16 +9,13 @@ class PreLayer(nn.Module):
         pre = d
 
         for x in hidden[:-1]:
-            self.net.extend([
-                nn.Linear(pre, x),
-                nn.BatchNorm1d(x),
-                nn.ReLU()
-            ])
+            self.net.extend([nn.Linear(pre, x), nn.BatchNorm1d(x), nn.ReLU()])
             pre = x
 
         self.net.append(nn.Linear(pre, hidden[-1]))
 
     def forward(self, x):
         for layer in self.net:
+            print(x.shape)
             x = layer(x)
         return x
